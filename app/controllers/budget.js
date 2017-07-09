95f6b8627a8f54910d90fa91081fd3ac63345eae
Gustavo San Jose <gustavo@coschedule.com>
2017-07-08 20:29:06 -0500

    [FEATURE] Added CPs for budget

---
+++ app/controllers/budget.js
@@ -0,0 +1,38 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  surveyRecord: Ember.inject.service(),
+  peopleInHousehold: 1,
+  incomesInHousehold: 1,
+  housingBudget: 300,
+  squareFeetPerPerson: 300,
+
+  totalSquareFootage: Ember.computed('peopleInHousehold','squareFeetPerPerson', function () {
+    return this.get('peopleInHousehold') * this.get('squareFeetPerPerson');
+  }),
+
+  costPerSquareFoot: Ember.computed('totalSquareFootage', 'totalBudget', function () {
+    return (this.get('totalBudget') / this.get('totalSquareFootage')).toFixed(2);
+  }),
+
+  totalBudget: Ember.computed('incomesInHousehold', 'housingBudget', function() {
+    return this.get('incomesInHousehold') * this.get('housingBudget');
+  }),
+
+  actions: {
+    continue() {
+      const currentSurvey = this.get('surveyRecord').current();
+      const {peopleInHousehold, incomesInHousehold, housingBudget, squareFeetPerPerson} = this.controller.getProperties(
+        'peopleInHousehold', 'incomesInHousehold', 'housingBudget', 'squareFeetPerPerson');
+
+      currentSurvey.setProperties({
+        peopleInHousehold,
+        incomesInHousehold,
+        housingBudget,
+        squareFeetPerPerson,
+      });
+
+      this.transitionTo('location');
+    },
+  },
+});
--- app/routes/budget.js
+++ app/routes/budget.js
@@ -1,22 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  surveyRecord: Ember.inject.service(),
 
-  actions: {
-    continue() {
-      const currentSurvey = this.get('surveyRecord').current();
-      const {peopleInHousehold, incomesInHousehold, housingBudget, squareFeetPerPerson} = this.controller.getProperties(
-        'peopleInHousehold', 'incomesInHousehold', 'housingBudget', 'squareFeetPerPerson');
-
-      currentSurvey.setProperties({
-        peopleInHousehold,
-        incomesInHousehold,
-        housingBudget,
-        squareFeetPerPerson,
-      });
-
-      this.transitionTo('location');
-    },
-  },
 });
