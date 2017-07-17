787a68861cc4070cc12e5101e573e4f06591e4ae
Gustavo San Jose <gustavo@coschedule.com>
2017-07-16 21:18:26 -0500

    [FEATURE] Got started on housing types

--- app/controllers/housing-type.js
+++ app/controllers/housing-type.js
@@ -1,4 +1,43 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  surveyRecord: Ember.inject.service(),
+
+  housingTypePreference: 'rehab',
+  housingType: 'largeLot',
+
+  housingTypes: [{
+      key: 'largeLot',
+      value: 'Large Lot'
+    }, {
+      key: 'smallLot',
+      value: 'Small Lot'
+    }, {
+      key: 'twinHome',
+      value: 'Twin Home',
+    }, {
+      key: 'rowHome',
+      value: 'Row Home',
+    }, {
+      key: 'accessoryDwelling',
+      value: 'Accessory Dweling Unit',
+    }, {
+      key: 'walkupFlat',
+      value: 'Walk-Up Flat',
+    }, {
+      key: 'midriseFlat',
+      value: 'Mid-Rise Flat'
+    }, {
+      key: 'highriseFlat',
+      value: 'High-Rise Flat',
+    },
+  ],
+
+  actions: {
+    changeType(newType) {
+      this.set('housingType', newType);
+    }
+  }
+
+
 });
