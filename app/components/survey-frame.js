e72866685e28c92962f569c71dfa4f7e3b55c181
Gustavo San Jose <gustavo@coschedule.com>
2017-08-12 21:58:33 -0500

    [FEATURE] Added linkto action from navbar

--- app/components/survey-frame.js
+++ app/components/survey-frame.js
@@ -3,4 +3,10 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   surveyRecord: Ember.inject.service(),
   router: Ember.inject.service(),
+
+  actions: {
+    goTo(routeName) {
+      this.get('router').transitionTo(routeName);
+    },
+  },
 });
