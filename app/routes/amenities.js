5908522e8d40d071e192fa498a82a5d2b38c384d
Gustavo San Jose <gustavo@coschedule.com>
2017-08-12 22:38:54 -0500

    [FEATURE] Added redirect hook to guard stupid behavior

--- app/routes/amenities.js
+++ app/routes/amenities.js
@@ -3,6 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   surveyRecord: Ember.inject.service(),
 
+  redirect() {
+    if (!this.get('surveyRecord.visitedDemographics')) {
+      this.transitionTo('demographics');
+    }
+  },
+
   activate() {
     this.get('surveyRecord').set('visitedAmenities', true);
   },
